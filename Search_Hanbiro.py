394973453edc519034701aa8aa44d34373fc0247
btj <tjback123@suresofttech.com>
2017-10-11 10:20:23 +0900

    이전 달, 다음 달 넘어가는 코드 구현

--- Search_Hanbiro.py
+++ Search_Hanbiro.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-
 # 한비로 로그인 주소
 web_address = 'http://suresofttech.hanbiro.net/groupware/login.php'
 calendar_address = 'http://suresofttech.hanbiro.net/groupware/?category=time&section=userCalendar'
@@ -15,14 +14,24 @@ driver.get(web_address)
 # 아이디 입력
 driver.find_element_by_name('hmail_id').send_keys('tjback123')
 # 비밀번호 입력
-driver.find_element_by_name('hmail_pass').send_keys('xxxx')
+driver.find_element_by_name('hmail_pass').send_keys('dowk0056')
 # 자동 로그인 시도
 driver.find_element_by_xpath('/html/body/form/table/tbody/tr[1]/td/table[1]/tbody/tr[2]/td/table/tbody/tr[2]/td[3]/table/tbody/tr/td').click()
 # 바로 접속하면 캘린더로 접속이 안되므로 3초의 여유를 둔다.
 time.sleep(3)
 # 캘린더로 접속
 driver.get(calendar_address)
+
+# 이전달로 이동
+# driver.find_element_by_xpath('/html/body/table/tbody/tr/td/table[2]/tbody/tr/td/span[1]').click()
+
+# 다음달로 이동
+# driver.find_element_by_xpath('/html/body/table/tbody/tr/td/table[2]/tbody/tr/td/span[2]').click()
+
 # 웹페이지 소스 추출
 get_html = driver.page_source
 # HTML 소스 읽어오기
 get_parser = BeautifulSoup(get_html, 'html.parser')
+# 출근 정보 태그
+get_tag = get_parser.find_all('img')
+
